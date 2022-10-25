dd6efba1449658415cdced2695b9181a1cf4a9a0
petarotovic <otovicpetar1998@gmail.com>
2022-10-24 18:32:09 -0700

    DZ04 Zadatak 2

---
+++ CS100-DZ04/Zadatak2/Zadatak2.py
@@ -0,0 +1,15 @@
+# Napisati zadatak koji od korisnika zahteva da unese jedan ceo broj manji od 20, n. Nakon toga
+# program ispisuje sve brojeve do unetog broja n, ali tako da se svaki broj ponavlja onoliko puta
+# kolika mu je vrednost.
+# Primer za n=5:
+# 1 2 2 3 3 3 4 4 4 4 5 5 5 5 5
+
+n = int(input("Ceo broj manji od 20 -> "))
+
+outputStr = ""
+
+for x in range(1, n + 1):
+    for y in range(x):
+        outputStr = outputStr + str(x) + " "
+
+print(outputStr)
