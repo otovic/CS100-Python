4f6fa7f5370c39b6030c5265dd832df875996f2d
PeraSniper <otovicpetar1998@gmail.com>
2022-11-04 20:42:56 +0100

    DZ05 Zadatak 3 Ispravka

--- CS100-DZ05/Zadatak3.py
+++ CS100-DZ05/Zadatak3.py
@@ -1,5 +1,4 @@
 # Napisati funkciju koja izračunava zapreminu sfere prema njenom poluprečniku.
 
 zapreminaSfere = lambda pp: 4*(pp*pp*pp)*3.14 / 3
-poluprecnik = float(input("Unesite poluprecnik -> "))
-print("Zapremina sfere je -> " + str(zapreminaSfere(poluprecnik)))
+print("Zapremina sfere je -> " + str(zapreminaSfere(int(input("Unesite poluprecnik -> ")))))
