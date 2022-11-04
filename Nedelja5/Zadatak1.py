a46a475314ca354218b7221478acff24ae62476c
petarotovic <otovicpetar1998@gmail.com>
2022-11-04 13:54:32 -0700

    IDV5 Zadatak 1 Ispravka

--- Nedelja5/Zadatak1.py
+++ Nedelja5/Zadatak1.py
@@ -1,7 +1,4 @@
 def thirdString(*text):
-    if len(text) < 3:
-        print("Unesite najmanje 3 argumenta!")
-    else:
-        print(text[2])
+    print("Unesite najmanje 3 argumenata") if len(text) < 3 else print(text[2])
 
 thirdString("petar1", "petar2", "petar3")
