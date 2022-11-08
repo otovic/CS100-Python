a2c5453a6ab42bd609aa9745dbc6790d3e7ebbf4
PeraSniper <otovicpetar1998@gmail.com>
2022-11-08 19:36:57 +0100

    DZ06 Zadatak 5

--- CS100-DZ06/Zadatak4/Zadatak4.py
+++ CS100-DZ06/Zadatak4/Zadatak4.py
@@ -2,3 +2,30 @@
 # Datoteka je takva da svaki red ima jednu reč. 
 # Zatim, preko standardnog ulaza ubacivati (red po red) reči treba promeniti u postojećoj datoteci. 
 # Koristiti kontekstni menadžer.
+
+# rec1
+# rec2
+# rec3
+# rec4
+
+with open("text.txt", "r") as f:
+    lines = f.readlines()
+
+    for x in range(len(lines)):
+        print(f"{x + 1}: " + lines[x].replace("\n", ''))
+
+f.close()
+
+print("Unesite zamene za reci u fajlu: \n")
+
+with open("text.txt", "w") as f:
+    for x in range(len(lines)):
+        if x < len(lines) - 1:
+            f.write(str(input(f"{x + 1} rec za zamenu -> ")) + "\n")
+        else:
+            f.write(str(input(f"{x + 1} rec za zamenu -> ")))
+f.close()
+
+with open("text.txt", "r") as f:
+    print("Novi fajl: \n" + f.read())
+f.close()
---
+++ CS100-DZ06/Zadatak5/Zadatak5.py
@@ -0,0 +1,17 @@
+# Napisati program koji kopira binarnu datoteku po blokovima. 
+# Veličina bloka se unosi preko standardnog ulaza i dozvoljene vrednosti su: 
+# 1024, 2048, 4096, 8192. Ukoliko korisnik ne unese neku od ovih vrednosti, vratiti poruku sa greškom. 
+# Koristiti kontekstni menadžer.
+
+validNums = [1024, 2048, 4096, 8192]
+velBloka = int(input("Unesite velicinu bloka (1024, 2048, 4096 ili 8192 -> "))
+
+if velBloka not in validNums:
+    print("Unesite validnu velicinu bloka!")
+else:
+    with open("poodle.jpg", "rb") as rf:
+        with open("poodleCopy.jpg", "wb") as wf:
+            x = rf.read(velBloka)
+            while len(x) > 0:
+                wf.write(x)
+                x = rf.read(velBloka)
