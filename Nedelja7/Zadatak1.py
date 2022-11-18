37cddf837f88a4bf2dba947587d8d136e46c17d0
petarotovic <otovicpetar1998@gmail.com>
2022-11-18 13:59:30 -0800

    Small Changes

--- Nedelja7/Zadatak1.py
+++ Nedelja7/Zadatak1.py
@@ -1,12 +1,11 @@
 #izracunati povrsinu i obim pravougaonika
 
 def povrsinaPravougaonika(a, b):
-    try:
-        if any(vrednost < 1 for vrednost in [a, b]):
-            raise Exception("Unesite ispravne vrednosti!")
-        else:
+    if any(vrednost < 1 for vrednost in [a, b]):
+        raise Exception("Unesite ispravne vrednosti!")
+    else:
             return a * b
-    except Exception as error:
-        print(error)
-
-print("Povrsina je: " + str(povrsinaPravougaonika(int(input("Prva stranica: ")), int(input("Druga stranica: ")))))
+try:
+    print("Povrsina je: " + str(povrsinaPravougaonika(int(input("Prva stranica: ")), int(input("Druga stranica: ")))))
+except Exception as error:
+    print(error)
