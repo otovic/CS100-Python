1277283833aa3589f2035c3c1e167af5f6f0e14d
PetarOtovic <otovicpetar1998@gmail.com>
2022-12-02 18:03:25 -0800

    Test

---
+++ Nedelja9/Zadatak3.py
@@ -0,0 +1,25 @@
+import matplotlib.pyplot as plt
+
+# plt.plot([3, 6, 5, 8], [0, 10, 0, 10], linestyle = 'dotted', color = 'red', linewidth = '20.5')
+# plt.plot([1, 2], [2,3])
+# plt.xlabel('Testy')
+# plt.ylabel('ampersent')
+# plt.title('grafikon', loc='left')
+# plt.grid(axis='x')
+
+# plt.subplot(2, 3, 1)
+# plt.plot([1, 2], [2, 3])
+
+# plt.subplot(2, 3, 2)
+# plt.plot([3, 3], [2, 3])
+
+# plt.subplot(2, 3, 3)
+# plt.plot([3, 3], [2, 3])
+
+# plt.subplot(2, 3, 4)
+# plt.plot([3, 3], [2, 3])
+
+# plt.suptitle('Tona djubreta')
+
+plt.bar(['Niska  Banja', 'Palilula', 'Medijana', 'Crveni Krst', 'Pantelej'], [4, 5, 10, 10, 10], width=0.2)
+plt.show()
