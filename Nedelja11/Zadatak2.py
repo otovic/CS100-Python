66d9621adea212302dd9e236cbb4c55e1b13365b
petarotovic <otovicpetar1998@gmail.com>
2022-12-16 13:52:03 -0800

    Nedelja11 Ispravka

--- Nedelja11/Zadatak2.py
+++ Nedelja11/Zadatak2.py
@@ -1,12 +1,10 @@
-reci = set()
-text = input("Unesite neki tekst: ").split()
-dict = {}
-for rec in text:
-    dict.setdefault(rec.lower(), 0)
-    dict[rec] += 1
+text = set(input("Unesite neki tekst: ").split())
+print(text)
+# dict = {}
+# for rec in text:
+#     dict.setdefault(rec.lower(), 0)
+#     dict[rec] += 1
 
-for x, y in dict.items():
-    if y == 1:
-        reci.add(x)
-
-print(reci)
+# for x, y in dict.items():
+#     if y == 1:
+#         reci.add(x)
