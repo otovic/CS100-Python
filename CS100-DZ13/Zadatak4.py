c63d48341a5eb469887769e8d142389194bf5d5e
PetarOtovic <otovicpetar1998@gmail.com>
2023-01-12 16:27:49 -0800

    DZ13 Zad 3 i 4

---
+++ CS100-DZ13/Zadatak4.py
@@ -0,0 +1,5 @@
+def ispisiNiz(stringovi):
+    for index, ime in enumerate(stringovi):
+        print(f"{ime} pozicija u listi: {index + 1}")
+
+ispisiNiz(['jedan', 'dva', 'tri', 'test', 'petar', 'petarrr', '5460'])
